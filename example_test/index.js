85f4f5623318d686035665f96300f9645d44eed8
Lukas <lukasdrinkwater97@gmail.com>
2023-04-22 20:30:12 +0100

    finished stopwatch practice

--- example_test/index.js
+++ example_test/index.js
@@ -1,5 +1,35 @@
 function Stopwatch() {
-  this.start = function () {};
+  let startTime, elapsedTime, duration, running;
+
+  this.start = function () {
+    if (running) throw new Error("Stopwatch has already started");
+    running = true;
+    startTime = new Date();
+    console.log(startTime);
+  };
+
+  this.stop = function () {
+    if (!running) throw new Error("Stopwatch has not started");
+    running = false;
+
+    elapsedTime = new Date() - startTime;
+
+    console.log(elapsedTime);
+  };
+
+  this.reset = function () {
+    startTime = undefined;
+    elapsedTime = undefined;
+    seconds = undefined;
+    duration = undefined;
+  };
+
+  // being an Object.defineProperty makes it a read only.
+  Object.defineProperty(this, "duration", {
+    get: function () {
+      return elapsedTime;
+    },
+  });
 }
 
 const sw = new Stopwatch();
